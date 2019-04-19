28e3e9a21f619ae73223d91fda8ce1031d418dcb
Kathan Master <kathan.master@yahoo.com>
2019-04-19 17:24:36 -0400

    Added init for sln

--- src/SingleLayerNet.py
+++ src/SingleLayerNet.py
@@ -7,6 +7,19 @@ class Neuron:
 class SingleLayerNet:
     def __init__(self, inputSize, layerSize, outputSize):
         self.inputLayer = [Neuron(0) for i in xrange(inputSize)]
+        self.hiddenLayer = [Neuron(0) for i in xrange(layerSize)]
+        self.outputLayer = [Neuron(0) for i in xrange(outputSize)]
+
+        weights = {}
+        for n1 in self.inputLayer:
+            for n2 in self.hiddenLayer:
+                weights[(n1, n2)] = 1
+
+        for n1 in self.hiddenLayer:
+            for n2 in self.outputLayer:
+                weights[(n1, n2)] = 1
+        self.weights = weights
+        
 
 sln = SingleLayerNet(3, 3, 3)
 print sln.inputLayer
