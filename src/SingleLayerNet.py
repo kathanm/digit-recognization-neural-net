50de6ece778aa431d522b254b6ec3a5bcc0e94b9
Kathan Master <kathan.master@yahoo.com>
2019-04-19 17:21:50 -0400

    added sln clas

--- src/SingleLayerNet.py
+++ src/SingleLayerNet.py
@@ -4,3 +4,9 @@ class Neuron:
     def __init__(self, bias):
         self.bias = bias
 
+class SingleLayerNet:
+    def __init__(self, inputSize, layerSize, outputSize):
+        self.inputLayer = [Neuron(0) for i in xrange(inputSize)]
+
+sln = SingleLayerNet(3, 3, 3)
+print sln.inputLayer
