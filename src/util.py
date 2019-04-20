6ba44cb4d7aeff9c92ad253f8c2a918bab0ab3aa
williehank <williehankvictoria@gmail.com>
2019-04-20 16:14:39 -0400

    Changed sigmoid function to avoid overflow

--- src/util.py
+++ src/util.py
@@ -2,7 +2,10 @@ import math
 
 
 def sigmoid(n):
-    return 1 / (1 + math.exp(-1 * n))
+    if n < 0:
+        return 1 - 1 / (1 + math.exp(n))
+    return 1 / (1 + math.exp(-n))
+
 
 def sigmoid_derivative(n):
     return sigmoid(n) * (1 - sigmoid(n))
