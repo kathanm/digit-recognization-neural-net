fixed backprop and added training method

src/SingleLayerNet.py
@@ -1,4 +1,7 @@
+import numpy as np
+
 import util
+import pickle
 
 class Neuron:
     def __init__(self, bias):
@@ -48,22 +51,61 @@ class SingleLayerNet:
             raise Exception()
         result = zip(self.outputLayer, expectedOutput)
 
+        hiddenLayerActivationDerviative = []
+        for i1, n1 in enumerate(self.hiddenLayer):
+            derivativeOfCostByActivation = 0
+            for i2, n2 in enumerate(self.outputLayer):
+                z = n1.value * self.weights[(n1, n2)] + n2.bias
+                derivativeByZ = util.sigmoid_derivative(z) * (n2.value - expectedOutput[i2])
+                gradient = n1.value * derivativeByZ
+                derivativeOfCostByActivation += self.weights[(n1, n2)] * derivativeByZ
+                n2.bias -= self.learningRate * derivativeByZ
+                self.weights[(n1, n2)] -= self.learningRate * gradient
+            hiddenLayerActivationDerviative.append(derivativeOfCostByActivation)
+
         for n in self.inputLayer:
             for i1, n1 in enumerate(self.hiddenLayer):
-                derivativeOfCostByActivation = 0
-                for i2, n2 in enumerate(self.outputLayer):
-                    z = n1.value * self.weights[(n1, n2)] + n2.bias
-                    derivativeByZ = util.sigmoid_derivative(z) * (n2.value - expectedOutput[i2])
-                    gradient = n1.value * derivativeByZ
-                    derivativeOfCostByActivation += self.weights[(n1, n2)] * derivativeByZ
-                    n2.bias -= self.learningRate * derivativeByZ
-                    self.weights[(n1, n2)] -= self.learningRate * gradient
                 z = n.value * self.weights[(n, n1)] + n1.bias
-                derivativeByZ = util.sigmoid_derivative(z) * derivativeOfCostByActivation
+                derivativeByZ = util.sigmoid_derivative(z) * hiddenLayerActivationDerviative[i1]
                 gradient = n.value * derivativeByZ
                 n1.bias -= self.learningRate * derivativeByZ
                 self.weights[(n, n1)] -= self.learningRate * gradient
 
 
 
-sln = SingleLayerNet(3, 3, 3)
+def main():
+    image_size = 28
+    no_of_different_labels = 10
+    image_pixels = image_size * image_size
+    train_data = np.loadtxt(
+        'I:\\Programing\\College\\AI\\Projects\\digit-recognization-neural-net\\resources\\train.csv', None, '#', ',')
+    test_data = np.loadtxt('I:\\Programing\\College\\AI\\Projects\\digit-recognization-neural-net\\resources\\test.csv',
+                           None, '#', ',')
+    fac = 0.99 / 255
+    train_imgs = np.asfarray(train_data[:, 1:]) * fac + 0.01
+    test_imgs = np.asfarray(test_data[:, 1:]) * fac + 0.01
+    train_labels = np.asfarray(train_data[:, :1])
+    test_labels = np.asfarray(test_data[:, :1])
+
+    lr = np.arange(no_of_different_labels)
+    train_labels_one_hot = (lr == train_labels).astype(np.float)
+    test_labels_one_hot = (lr == test_labels).astype(np.float)
+    train_labels_one_hot[train_labels_one_hot == 0] = 0.01
+    train_labels_one_hot[train_labels_one_hot == 1] = 0.99
+    test_labels_one_hot[test_labels_one_hot == 0] = 0.01
+    test_labels_one_hot[test_labels_one_hot == 1] = 0.99
+
+    sln = SingleLayerNet(image_pixels, 20, no_of_different_labels)
+    count = 0
+    for i in range(len(train_imgs)):
+        sln.readInput(train_imgs[i])
+        sln.feedForward()
+        sln.backProp(train_labels_one_hot[i])
+        print count
+        count = count + 1
+
+    with open('sln.pkl', 'wb') as output:
+        pickle.dump(sln, output, pickle.HIGHEST_PROTOCOL)
+
+
+main()