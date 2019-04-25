adc4bc8bf9612617e962039e69716c302c53d673
Kathan Master <kathan.master@yahoo.com>
2019-04-25 04:54:40 -0400

    fixed typo

--- src/util.py
+++ src/util.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def sigmoid(n):
-    return 1.0 / (1.0 + np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-n))
 
 def sigmoid_derivative(n):
     return sigmoid(n) * (1 - sigmoid(n))
